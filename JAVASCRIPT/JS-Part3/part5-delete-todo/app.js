e8bd3e0b9d395e064d5ca6bc0a2958de350b461c
MaliBYK <malibyk00@gmail.com>
2021-02-19 15:54:12 +0300

    deleting element

---
+++ JAVASCRIPT/JS-Part3/part5-delete-todo/app.js
@@ -0,0 +1,12 @@
+const todoList = document.querySelector("ul.list-group");
+const todos = document.querySelectorAll("li.list-group-item");
+console.log(todoList);
+console.log(todos);
+
+// todos[0].remove();
+// console.log(todoList.children);
+
+// todoList.removeChild(todoList.children[todoList.children.length - 1]);
+// todoList.children[todoList.children.length - 1].remove();
+
+//remove method
