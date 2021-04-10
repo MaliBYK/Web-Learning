40becf4a83e985bf82e1a50913a596d1aec8242f
MaliBYK <developerbox00@gmail.com>
2021-04-10 22:46:17 +0300

    Project Finished

--- JAVASCRIPT/JS-Part15-Employee-Project/src/index.js
+++ JAVASCRIPT/JS-Part15-Employee-Project/src/index.js
@@ -1,19 +1,24 @@
-import { Request } from "./requests";
-const request = new Request("http://localhost:3000/employees");
-
-import { UI } from "./ui";
-const ui = new UI();
-
+const tbody = document.querySelector("#employees");
 const table = document.querySelector(".table.table-dark");
 const addButton = document.querySelector("#add-employee");
 const nameInput = document.querySelector("#name");
 const departmentInput = document.querySelector("#department");
 const salaryInput = document.querySelector("#salary");
+const updateButton = document.querySelector("#update");
+
+let updateState = null;
+
+import { Request } from "./requests";
+const request = new Request("http://localhost:3000/employees");
+
+import { UI } from "./ui";
+const ui = new UI(nameInput, departmentInput, salaryInput, tbody, updateButton);
 
 function addEventListeners() {
   addButton.addEventListener("click", addEmployee);
   document.addEventListener("DOMContentLoaded", loadAllEmployees);
   table.addEventListener("click", deleteOrUpdate);
+  updateButton.addEventListener("click", updateEmployee);
 }
 
 function deleteOrUpdate(e) {
@@ -22,7 +27,7 @@ function deleteOrUpdate(e) {
     return;
   }
   if (e.target.id === "update-employee") {
-    //Update Employee
+    updateEmployeeController(e);
   }
 }
 
@@ -43,6 +48,8 @@ function addEmployee() {
       salary: salaryInput.value,
     })
     .then(employee => ui.addEmployee(employee));
+
+  ui.clearInputs();
 }
 
 function deleteEmployee(targetEmployee) {
@@ -50,6 +57,34 @@ function deleteEmployee(targetEmployee) {
   request.delete(id);
 
   ui.deleteElementFromUI(targetEmployee.parentElement.parentElement);
+}
+
+function updateEmployeeController(e) {
+  const parentElement = e.target.parentElement.parentElement;
+  const id = ui.getIdOfElement(parentElement);
+
+  ui.showInfosInInputs(parentElement);
+  ui.toggleUpdateButton();
+
+  if (updateState === null) {
+    updateState = {
+      id: id,
+      parentElement: parentElement,
+    };
+  } else updateState = null;
+}
+
+function updateEmployee() {
+  if (updateState) {
+    const data = {
+      name: nameInput.value,
+      department: departmentInput.value,
+      salary: salaryInput.value,
+    };
+    request.put(data, updateState.id).then(updatedElement => {
+      ui.updateEmployeeOnUI(updatedElement, updateState.parentElement);
+    });
+  }
 }
 
 addEventListeners();
--- JAVASCRIPT/JS-Part15-Employee-Project/src/ui.js
+++ JAVASCRIPT/JS-Part15-Employee-Project/src/ui.js
@@ -1,7 +1,14 @@
-const tbody = document.querySelector("#employees");
 export class UI {
+  constructor(nameInput, departmentInput, salaryInput, tbody, updateButton) {
+    this.updateButton = updateButton;
+    this.nameInput = nameInput;
+    this.departmentInput = departmentInput;
+    this.salaryInput = salaryInput;
+    this.tbody = tbody;
+  }
+
   addEmployee(employee) {
-    tbody.innerHTML += `
+    this.tbody.innerHTML += `
     <tr>
         <td>${employee.name}</td>
         <td>${employee.department}</td>
@@ -15,5 +22,57 @@ export class UI {
 
   deleteElementFromUI(targetElement) {
     targetElement.remove();
+  }
+
+  getIdOfElement(targetElement) {
+    const id = targetElement.children[3].textContent;
+    return id;
+  }
+
+  showInfosInInputs(targetElement) {
+    const children = targetElement.children;
+    this.nameInput.value = children[0].textContent;
+    this.departmentInput.value = children[1].textContent;
+    this.salaryInput.value = children[2].textContent;
+  }
+
+  toggleUpdateButton() {
+    if (this.updateButton.style.display === "none") {
+      this.updateButton.style.display = "block";
+      return;
+    }
+    this.updateButton.style.display = "none";
+    this.clearInputs();
+  }
+
+  clearInputs() {
+    this.nameInput.value = "";
+    this.departmentInput.value = "";
+    this.salaryInput.value = "";
+  }
+
+  getInputInfos() {
+    const employee = {
+      name: this.nameInput.value.trim(),
+      department: this.departmentInput.value.trim(),
+      salary: this.salaryInput.value.trim(),
+    };
+    return employee;
+  }
+
+  updateEmployeeOnUI(updatedElement, parentElement) {
+    parentElement.innerHTML = `
+    <tr>
+      <td>${updatedElement.name}</td>
+      <td>${updatedElement.department}</td>
+      <td>${updatedElement.salary}</td>
+      <td class = "employee-id">${updatedElement.id}</td>
+      <td><a href="#" id = "update-employee" class= "btn btn-danger">Güncelle</a></td> 
+      <td> <a href="#" id = "delete-employee" class= "btn btn-danger">Sil</a></td>
+    </tr>
+    `;
+
+    this.clearInputs();
+    this.toggleUpdateButton();
   }
 }
